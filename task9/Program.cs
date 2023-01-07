84eb8632d531fea419e5bb347a45fd43c0c4ca25
VyacheslavVolkhin <vv@htmlworker.com>
2023-01-07 17:46:07 +0500

    seminar

---
+++ task9/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+Clear();
+
+// int number = new Random().Next(100, 1000);
+// int a1 = number/100;
+// int a2 = number%10;
+// WriteLine(number);
+// WriteLine($"{a1},{a2}");
+
+// WriteLine("Enter num1: ");
+// int a1 = int.Parse(ReadLine()!);
+// int a2 = 7;
+// int a3 = 23;
+
+// WriteLine(a1%a2==0 && a1%a3==0?"Yes":$"No");
+
+WriteLine("Enter num1: ");
+int a1 = int.Parse(ReadLine()!);
+WriteLine("Enter num2: ");
+int a2 = int.Parse(ReadLine()!);
+
+WriteLine(a1*a1==a2?"a1^a1=a2":a2*a2==a1?"a2^a2=a1":"No");
