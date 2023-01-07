370f82de07ae3501bb8e24a17409dd3eaa9c5352
VyacheslavVolkhin <vv@htmlworker.com>
2023-01-07 17:54:52 +0500

    Решение задачи 10

---
+++ HomeWork2/Task 10/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите трехзначное число: ");
+int number = int.Parse(ReadLine()!);
+
+while (number<100 || number>999) {
+    WriteLine("Введите трехзначное число: ");
+    number = int.Parse(ReadLine()!);
+}
+
+int a = (number%100)/10;
+
+WriteLine($"Вторая цифра в этом числе {a}");
