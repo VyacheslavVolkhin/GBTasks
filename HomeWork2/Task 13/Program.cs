72c05581ec2e35430993f0cc63e395c71eb0f648
VyacheslavVolkhin <vv@htmlworker.com>
2023-01-07 18:00:48 +0500

    Решение задачи 13

---
+++ HomeWork2/Task 13/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите число не больше 999: ");
+int number = int.Parse(ReadLine()!);
+
+while (number>999) {
+    WriteLine("Введите число не больше 999: ");
+    number = int.Parse(ReadLine()!);
+}
+
+WriteLine(number>99?$"Третья цифра в этом числе {number%10}":"Третьей цифры нет ((");
