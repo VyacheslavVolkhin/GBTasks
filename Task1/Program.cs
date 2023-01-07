cf69eb016c4ccc0db7b6dd148068c2717ba8fa48
VyacheslavVolkhin <vv@htmlworker.com>
2023-01-07 16:06:30 +0500

    Решение первой задачи

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+
+WriteLine("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+
+if (a == (b*b)) {
+    WriteLine($"Число {a} является квадратом числа {b}");
+} else {
+    WriteLine($"Число {a} не является квадратом числа {b}");
+}
