6963b031d61bc6d0050e2b4b8672ad69177310f6
VyacheslavVolkhin <vv@htmlworker.com>
2023-01-07 16:12:59 +0500

    решение второй задачи

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Enter digit: ");
+int a = int.Parse(ReadLine()!);
+
+if (a == 1) {
+    WriteLine("Monday");
+} else if (a == 2) {
+    WriteLine("Tuesday");
+} else if (a == 3) {
+    WriteLine("Wednesday");
+} else if (a == 4) {
+    WriteLine("Thursday");
+} else if (a == 5) {
+    WriteLine("Friday");
+} else if (a == 6) {
+    WriteLine("Saturday");
+} else if (a == 7) {
+    WriteLine("Sunday");
+} else {
+    WriteLine("No week day");
+}
