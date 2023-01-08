1742588ef13c00e75ab22fe1b750a52042889545
VyacheslavVolkhin <vv@htmlworker.com>
2023-01-08 08:35:23 +0500

    Решение задачи 23

--- HomeWork3/Task 23/Program.cs
+++ HomeWork3/Task 23/Program.cs
@@ -1,2 +1,16 @@
 ﻿using static System.Console;
+using static System.Math;
 Clear();
+
+WriteLine("Введите целое число: ");
+if (!int.TryParse(ReadLine(), out int number)) 
+{
+    WriteLine("Ошибка");
+}
+int i = 1;
+
+while (i <= number)
+{
+    WriteLine($"{i} в кубе равно {Pow(i, 3)}");
+    i+=1;
+}
