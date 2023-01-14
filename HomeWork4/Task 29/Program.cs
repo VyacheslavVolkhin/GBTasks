d1a991c212e4d8ee7840f98562f49355174f42b2
VyacheslavVolkhin <vv@htmlworker.com>
2023-01-14 20:22:36 +0500

    Решение задачи 29

--- HomeWork4/Task 29/Program.cs
+++ HomeWork4/Task 29/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+using static System.Math;
+
+Clear();
+
+
+void GetArray()
+{
+    int[] res = new int[8];
+    int count = 0;
+    while (count < res.Length)
+    {
+        res[count] = new Random().Next(0, 999);
+        count++;
+    }
+    Write("[ ");
+    for (int i = 0; i < res.Length; i++)
+    {
+        Write($"{res[i]} ");
+    }
+    Write(" ] ");
+    WriteLine();
+}
+
+GetArray();
