ca58b21cf16e69ecb652ccff4523cf800c934b55
VyacheslavVolkhin <vv@htmlworker.com>
2023-01-14 20:06:58 +0500

    Решение задачи 27

--- HomeWork4/Task 27/Program.cs
+++ HomeWork4/Task 27/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+using static System.Math;
+
+Clear();
+
+
+
+WriteLine("Enter A:");
+int A = int.Parse(ReadLine()!);
+
+WriteLine(GetSumm(A));
+
+int GetSumm(int a)
+{
+    int res = 0;
+    int lastDigit = 0;
+    while (a > 0)
+    {
+        lastDigit = a%10;
+        res +=lastDigit;
+        a /=10;
+    }
+    return res;
+}
