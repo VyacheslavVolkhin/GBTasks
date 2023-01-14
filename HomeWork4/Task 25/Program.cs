77d7078e5a409fe1ca94f63659bd5bf0cbfecb86
VyacheslavVolkhin <vv@htmlworker.com>
2023-01-14 20:01:26 +0500

    Решение задачи 25

--- HomeWork4/Task 25/Program.cs
+++ HomeWork4/Task 25/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+using static System.Math;
+
+Clear();
+
+
+
+WriteLine("Enter A:");
+int A = int.Parse(ReadLine()!);
+
+WriteLine("Enter B:");
+int B = int.Parse(ReadLine()!);
+
+WriteLine(PowNumbers(A, B));
+
+int PowNumbers(int a, int b)
+{
+    int res = A;
+    int i = 1;
+    while (i < b)
+    {
+        res = res*A;
+        i++;
+    }
+    return res;
+}
